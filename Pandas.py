eeef4cebb2970f49efa24e7dda61d19633c0cb41
phperesdasilva <phperesdasilva@gmail.com>
2022-01-24 21:06:16 -0300

    aula 29

--- Pandas.py
+++ Pandas.py
@@ -26,14 +26,14 @@ df = pd.DataFrame(np.random.randn(5,4), index='A B C D E'.split())    #split() -
 #criação de coluna no df
 df['novo'] = df[0] + df[1]
 
-print(df)
+#print(df)
 
 #tirar itens do dataframe
 #df = df.drop('novo', axis=1) #eixo de referencia axis=0 (indices)
 
 #ou
 
-df.drop('novo', axis=1, inplace=True)
+df.drop('novo', axis=1, inplace=True) #inplace substitui o df original pelo df criado 
 
 #localizar valores 
 df.loc['B']
@@ -44,3 +44,34 @@ df.loc[['A','B'],[0,1]]
 df.iloc[1]
 df.iloc[1:2,0:1]
 df.iloc[0:2,0:2]
+
+#criação de df
+df1 = pd.DataFrame(np.random.randn(5,4), index='A B C D E'.split(), columns='W X Y Z'.split())
+print(df1)
+
+#teste condicional de df
+df2 = df1>0
+#print(df2)
+
+#print(df1[df2]) #retorna df testado pela condicional 
+
+#print('\t')
+#print(df1[df1['Z']>0]) #retorna df somente com as linhas onde Z > 0
+
+#print('\t')
+#print(df1[df1['Z']>0]['Y']) #retorna a coluna Y somente com as lihas nas quais Z > 0
+
+#print('\t')
+#print(df1[df1<0]['W']) #retorna a coluna W com resultados negativos e resultados positivos mostrados como NaN
+
+#print('\t')
+#print(df1[(df1['W']>0) & (df1['Y']<0)]) #duas condicionais com lógica E
+#print(df1[(df1['W']>0) | (df1['Y']<0)]) #duas condicionais com lógica OU
+
+#df1.reset_index() #reseta os indices para números e cria uma coluna "index" na qual estão os índices em letras
+
+col = 'A1 B2 C3 D4 E5'.split()
+
+df1['COD'] = col
+
+df1.set_index('COD', inplace=True) #muda o índice do df para a coluna determinada
